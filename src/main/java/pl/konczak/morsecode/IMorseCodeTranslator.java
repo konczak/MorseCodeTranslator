9c7ace9e70ff709a755d397a91eeaafbd45ce569
Piotr Konczak <konczak.piotrek@gmail.com>
2015-04-12 18:03:17 +0200

    add IMorseCodeTranslator javadocs

--- src/main/java/pl/konczak/morsecode/IMorseCodeTranslator.java
+++ src/main/java/pl/konczak/morsecode/IMorseCodeTranslator.java
@@ -2,7 +2,25 @@ package pl.konczak.morsecode;
 
 public interface IMorseCodeTranslator {
 
+    /**
+     * Translates English text into Morse code.
+     *
+     * @param english input text. Allowed characters are " ,.?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" but upper case
+     * characters are replaced with their lower case equivalent.
+     * @return encoded String with MorseCode where single space indicates end of the letter and two subsequent spaces indicates end of the
+     * word.
+     * @throws IllegalArgumentException when input is null or contains unsupported characters
+     */
     String encode(String english);
 
+    /**
+     * Translates Morse code into English text.
+     *
+     * @param morseCode input Morse code. Single space indicates end of the letter/digit/character and two subsequent spaces indicates end
+     * of the word.
+     * @return decoded String with English text. Note Morse code does not recognize size of the letters so output will be always written
+     * with lower case characters.
+     * @throws IllegalArgumentException when input is null or contains unsupported characters
+     */
     String decode(String morseCode);
 }
