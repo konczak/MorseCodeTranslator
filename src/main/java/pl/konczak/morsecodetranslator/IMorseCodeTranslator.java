cb5caeab58b0c81087c4ba047e5f591a900a0d9a
Piotr Konczak <konczak.piotrek@gmail.com>
2015-04-12 18:23:03 +0200

    add to javadoc info about trimming narrow spaces

--- src/main/java/pl/konczak/morsecodetranslator/IMorseCodeTranslator.java
+++ src/main/java/pl/konczak/morsecodetranslator/IMorseCodeTranslator.java
@@ -6,7 +6,7 @@ public interface IMorseCodeTranslator {
      * Translates English text into Morse code.
      *
      * @param english input text. Allowed characters are " ,.?0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" but upper case
-     * characters are replaced with their lower case equivalent.
+     * characters are replaced with their lower case equivalent. Also it trims narrow spaces.
      * @return encoded String with MorseCode where single space indicates end of the letter and two subsequent spaces indicates end of the
      * word.
      * @throws IllegalArgumentException when input is null or contains unsupported characters
@@ -17,7 +17,7 @@ public interface IMorseCodeTranslator {
      * Translates Morse code into English text.
      *
      * @param morseCode input Morse code. Single space indicates end of the letter/digit/character and two subsequent spaces indicates end
-     * of the word.
+     * of the word. Narrow spaces are trimed.
      * @return decoded String with English text. Note Morse code does not recognize size of the letters so output will be always written
      * with lower case characters.
      * @throws IllegalArgumentException when input is null or contains unsupported characters
